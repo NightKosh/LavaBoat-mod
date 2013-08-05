96c1f87c9ec7006bbf3300ad11a2d3347f194d2b
NightKosh <nightmare@railfox.ru>
2013-08-05 22:44:47 +0400

    refactor mod info and resources

---
+++ Resources.java
@@ -0,0 +1,33 @@
+
+package LavaBoat;
+
+/*
+ * LavaBoat mod
+ *
+ * @author NightKosh
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ */
+public class Resources {
+    
+    private Resources() {
+        
+    }
+    
+    private static final String MOD_ID = ModInfo.ID;//.toLowerCase();
+    
+    // items
+    public static final String REINFORCES_BOAT        = MOD_ID + ":reinforcedBoat";
+    public static final String DOUBLE_REINFORCED_BOAT = MOD_ID + ":doubleReinforcedBoat";
+    public static final String CARGO_BOAT             = MOD_ID + ":cargoReinforcedBoat";
+    public static final String LAVA_BOAT              = MOD_ID + ":lavaBoat";
+    public static final String DOUBLE_LAVA_BOAT       = MOD_ID + ":doubleLavaBoat";
+    public static final String CARGO_LAVA_BOAT        = MOD_ID + ":cargoLavaBoat";
+    
+    // models
+    public static final String MODEL_REINFORCES_BOAT        = "/mods/LavaBoat/textures/ReinforcedBoat.png";
+    public static final String MODEL_DOUBLE_REINFORCED_BOAT = "/mods/LavaBoat/textures/ReinforcedDoubleBoat.png";
+    public static final String MODEL_CARGO_REINFORCED_BOAT  = "";
+    public static final String MODEL_LAVA_BOAT              = "/mods/LavaBoat/textures/LavaBoat.png";
+    public static final String MODEL_DOUBLE_LAVA_BOAT       = "/mods/LavaBoat/textures/DoubleLavaBoat.png";
+    public static final String MODEL_CARGO_LAVA_BOAT        = "";
+}
